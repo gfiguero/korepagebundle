2ad0df2530878aad9df9da05630dd3f7ef6af666
Garri <gfiguero@gmail.com>
2017-05-05 15:50:25 +0000

    commit desde el celular

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -4,10 +4,10 @@ namespace Kore\PageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class DashboardController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('KorePageBundle:Default:index.html.twig');
+        return $this->render('KorePageBundle:Dashboard:index.html.twig');
     }
 }
